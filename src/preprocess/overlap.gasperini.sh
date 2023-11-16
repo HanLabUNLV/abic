8a26b9f7108192099295ddcad64054306deee1fa
Mira Han <mira.han@unlv.edu>
2023-11-15 16:22:57 -0800

    preprocessing gasperini for EP pairs and genes

---
+++ src/preprocess/overlap.gasperini.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/bash
+set -uex
+
+DATADIR=data/Gasperini
+CRISPRFILE=/data8/han_lab/mhan/abic/$DATADIR/Gasperini2019.at_scale_screen.cand_enhancer_x_exprsd_genes.200503.csv
+ABCOUTDIR=/data8/han_lab/mhan/ABC-Enhancer-Gene-Prediction.bak/Gasperini/ABC_output
+TFFILE=/data8/han_lab/mhan/abic/data/ucsc/encRegTfbsClusteredWithK562.hg19.bed
+awk -F"," '{print $3"\t"$4"\t"$5"\t"$2}' $CRISPRFILE  | sed 's/"//g' | awk -F":" 'NR>1 {print $1}' | sort -k1,1 -k2,2n -k3,3n | uniq > $DATADIR/Gasperini2019.enhancer.bed
+awk -F"," '{print $6"\t"$7-500"\t"$8+500"\t"$14"\t0\t"$10}' $CRISPRFILE | sed 's/"//g' | awk 'NR>1' | sort -k1,1 -k2,2n -k3,3n | uniq > $DATADIR/Gasperini2019.TSS.bed
+
+echo -e "G.chr\tG.start\tG.end\tG.id\tABC.chr\tABC.start\tABC.end\tABC.class\tABC.id\toverlap" > $DATADIR/Gasperini2019.enhancer.ABC.overlap.bed
+bedtools intersect -wo -e -f 0.3 -F 0.3 -a $DATADIR/Gasperini2019.enhancer.bed -b $ABCOUTDIR/Neighborhoods/EnhancerList.bed | sed 's/|/\t/' >> $DATADIR/Gasperini2019.enhancer.ABC.overlap.bed
+ 
+awk '{print $4"\t"$0}' $DATADIR/Gasperini2019.TSS.bed | sort -k1,1 > /tmp/Gasperini.TSS
+awk '{print $4"\t"$0}' $ABCOUTDIR/Neighborhoods/GeneList.TSS1kb.bed | sort -k1,1 > /tmp/ABC.TSS
+echo -e "G.chr\tG.start\tG.end\tgene\tscore\tstrand\tABC.chr\tABC.start\tABC.end" > $DATADIR/Gasperini2019.TSS.ABC.overlap.bed
+join -a 1 /tmp/Gasperini.TSS /tmp/ABC.TSS  | awk '{print $2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10}' >> $DATADIR/Gasperini2019.TSS.ABC.overlap.bed
+
+awk '{print $1":"$2"-"$3"_"$7"\t"$0}' $ABCOUTDIR/Predictions/EnhancerPredictionsAllPutative.txt > $DATADIR/ABC.EnhancerPredictionsAllPutative.txt
+
+
+# TF
+bedtools intersect -wo -e -f 0.3 -F 0.3 -a $DATADIR/Gasperini2019.enhancer.bed -b $TFFILE | sed 's/|/\t/' >> $DATADIR/Gasperini2019.enhancer.TF.overlap.bed
+bedtools intersect -wo -e -f 0.3 -F 0.3 -a $DATADIR/Gasperini2019.TSS.bed -b $TFFILE | sed 's/|/\t/' >> $DATADIR/Gasperini2019.TSS.TF.overlap.bed
+
+
+#ln -s $ABCOUTDIR/Neighborhoods/EnhancerList.txt ${DATADIR}/EnhancerList.txt 
+#ln -s $ABCOUTDIR/Neighborhoods/GeneList.txt ${DATADIR}/GeneList.txt 
+#
+#ln -s $ABCOUTDIR/Neighborhoods.H3K4me3/EnhancerList.txt ${DATADIR}/EnhancerList.H3K4me3.txt 
+#ln -s $ABCOUTDIR/Neighborhoods.H3K4me3/GeneList.txt ${DATADIR}/GeneList.H3K4me3.txt 
+#
+#ln -s $ABCOUTDIR/Neighborhoods.H3K27me3/EnhancerList.txt ${DATADIR}/EnhancerList.H3K27me3.txt 
+#ln -s $ABCOUTDIR/Neighborhoods.H3K27me3/GeneList.txt ${DATADIR}/GeneList.H3K27me3.txt 
+
+
+# run overlap.gasperini.py
+# run groupbypos.py
+
+
+grep -v 'chr5' $DATADIR/Gasperini2019.at_scale.ABC.TF.cobinding.erole.grouped.txt | grep -v 'chr10' | grep -v 'chr15' | grep -v 'chr20' > $DATADIR/Gasperini2019.at_scale.ABC.TF.cobinding.erole.grouped.train.txt
+
+head -n 1 $DATADIR/Gasperini2019.at_scale.ABC.TF.cobinding.erole.grouped.train.txt > $DATADIR/Gasperini2019.at_scale.ABC.TF.cobinding.erole.grouped.test.txt
+grep 'chr5' $DATADIR/Gasperini2019.at_scale.ABC.TF.cobinding.erole.grouped.txt  >> $DATADIR/Gasperini2019.at_scale.ABC.TF.cobinding.erole.grouped.test.txt
+grep 'chr10' $DATADIR/Gasperini2019.at_scale.ABC.TF.cobinding.erole.grouped.txt  >> $DATADIR/Gasperini2019.at_scale.ABC.TF.cobinding.erole.grouped.test.txt 
+grep 'chr15' $DATADIR/Gasperini2019.at_scale.ABC.TF.cobinding.erole.grouped.txt  >> $DATADIR/Gasperini2019.at_scale.ABC.TF.cobinding.erole.grouped.test.txt
+grep 'chr20' $DATADIR/Gasperini2019.at_scale.ABC.TF.cobinding.erole.grouped.txt  >> $DATADIR/Gasperini2019.at_scale.ABC.TF.cobinding.erole.grouped.test.txt
+
+awk -F"\t"  '{print $23}' $DATADIR/Gasperini2019.at_scale.ABC.TF.cobinding.erole.grouped.test.txt > $DATADIR/Gasperini2019.at_scale.ABC.TF.cobinding.erole.grouped.test.target.txt
+
+
+grep -v 'chr5' $DATADIR/Gasperini2019.bygene.ABC.TF.grouped.txt | grep -v 'chr10' | grep -v 'chr15' | grep -v 'chr20' > $DATADIR/Gasperini2019.bygene.ABC.TF.grouped.train.txt
+
+head -n 1 $DATADIR/Gasperini2019.bygene.ABC.TF.grouped.train.txt > $DATADIR/Gasperini2019.bygene.ABC.TF.grouped.test.txt
+grep 'chr5' $DATADIR/Gasperini2019.bygene.ABC.TF.grouped.txt  >> $DATADIR/Gasperini2019.bygene.ABC.TF.grouped.test.txt
+grep 'chr10' $DATADIR/Gasperini2019.bygene.ABC.TF.grouped.txt  >> $DATADIR/Gasperini2019.bygene.ABC.TF.grouped.test.txt 
+grep 'chr15' $DATADIR/Gasperini2019.bygene.ABC.TF.grouped.txt  >> $DATADIR/Gasperini2019.bygene.ABC.TF.grouped.test.txt
+grep 'chr20' $DATADIR/Gasperini2019.bygene.ABC.TF.grouped.txt  >> $DATADIR/Gasperini2019.bygene.ABC.TF.grouped.test.txt
+
+awk -F"\t"  '{print $4}' $DATADIR/Gasperini2019.bygene.ABC.TF.grouped.test.txt > $DATADIR/Gasperini2019.bygene.ABC.TF.grouped.test.target.txt
+
---
+++ src/preprocess/overlap.gene.gasperini.py
@@ -0,0 +1,152 @@
+import pandas as pd
+import numpy as np
+
+# ABC
+data_dir = "data/Gasperini/"
+Gasperini_enhancer = pd.read_csv(data_dir+"Gasperini2019.enhancer.ABC.overlap.bed", sep='\t')
+Gasperini_TSS = pd.read_csv(data_dir+"Gasperini2019.TSS.ABC.overlap.bed", sep='\t')
+Gasperini_atscale = pd.read_csv(data_dir+"Gasperini2019.at_scale_screen.cand_enhancer_x_exprsd_genes.200503.csv")
+ABC = pd.read_csv(data_dir+"ABC.EnhancerPredictionsAllPutative.txt", sep='\t')
+ABC['ABC.Score.Denominator'] = ABC['ABC.Score.Numerator']/ABC['ABC.Score']
+ABC_by_gene = ABC.groupby('TargetGene')
+ABC_by_gene[["ABC.Score.Denominator"]].mean()
+def replaceNA(group):
+    mask = group.isna()
+    # Select those values where it is NA, and replace
+    # them with the mean of the values which are not NA.
+    group[mask] = group[~mask].mean()
+    return group
+Denominator_NA_replaced = ABC_by_gene["ABC.Score.Denominator"].transform(replaceNA)
+ABC['ABC.Score.Denominator'] = Denominator_NA_replaced
+
+new = Gasperini_atscale["name"].str.split(":", n=1, expand=True)
+Gasperini_atscale["enhancerID"]= new[0]
+Gasperini_enhancer = Gasperini_enhancer[["G.id", "ABC.id"]]
+
+ABC_enhancer = pd.read_csv(data_dir+"EnhancerList.txt", sep="\t")
+ABC_enhancer = ABC_enhancer.loc[:,['chr','start','end','name','class', 'normalized_h3K27ac', 'normalized_dhs', 'activity_base']]
+ABC_enhancer["ABC.id"] = ABC_enhancer['chr']+":"+ABC_enhancer['start'].astype(str)+"-"+ABC_enhancer['end'].astype(str)
+ABC_enhancer_H3K4me3 = pd.read_csv(data_dir+"EnhancerList.H3K4me3.txt", sep="\t")
+ABC_enhancer_H3K4me3.rename({'normalized_h3K27ac': 'normalized_h3K4me3'}, axis=1, inplace=True)
+ABC_enhancer_H3K4me3 = ABC_enhancer_H3K4me3.loc[:,['chr','start','end','normalized_h3K4me3']]
+ABC_enhancer_H3K4me3["ABC.id"] = ABC_enhancer_H3K4me3['chr']+":"+ABC_enhancer_H3K4me3['start'].astype(str)+"-"+ABC_enhancer_H3K4me3['end'].astype(str)
+ABC_enhancer_H3K4me3.drop(columns=['chr','start','end'], axis=1, inplace=True)
+ABC_enhancer_H3K27me3 = pd.read_csv(data_dir+"EnhancerList.H3K27me3.txt", sep="\t")
+ABC_enhancer_H3K27me3.rename({'normalized_h3K27ac': 'normalized_h3K27me3'}, axis=1, inplace=True)
+ABC_enhancer_H3K27me3 = ABC_enhancer_H3K27me3.loc[:,['chr','start','end','normalized_h3K27me3']]
+ABC_enhancer_H3K27me3["ABC.id"] = ABC_enhancer_H3K27me3['chr']+":"+ABC_enhancer_H3K27me3['start'].astype(str)+"-"+ABC_enhancer_H3K27me3['end'].astype(str)
+ABC_enhancer_H3K27me3.drop(columns=['chr','start','end'], axis=1, inplace=True)
+
+Gasperini_enhancer = pd.merge(Gasperini_enhancer, ABC_enhancer, left_on=["ABC.id"], right_on=["ABC.id"], suffixes=('', '_y'))
+Gasperini_enhancer.drop(Gasperini_enhancer.filter(regex='_y$').columns, axis=1, inplace=True)
+Gasperini_enhancer = pd.merge(Gasperini_enhancer, ABC_enhancer_H3K4me3, left_on=["ABC.id"], right_on=["ABC.id"], suffixes=('', '_y'))
+Gasperini_enhancer.drop(Gasperini_enhancer.filter(regex='_y$').columns, axis=1, inplace=True)
+Gasperini_enhancer = pd.merge(Gasperini_enhancer, ABC_enhancer_H3K27me3, left_on=["ABC.id"], right_on=["ABC.id"], suffixes=('', '_y'))
+Gasperini_enhancer.drop(Gasperini_enhancer.filter(regex='_y$').columns, axis=1, inplace=True)
+
+Gasperini_atscale2 = pd.merge(Gasperini_atscale, Gasperini_enhancer, left_on=["enhancerID"], right_on=["G.id"], suffixes=('', '_y'))
+Gasperini_atscale2.drop(Gasperini_atscale2.filter(regex='_y$').columns, axis=1, inplace=True)
+Gasperini_atscale2["ABC.id"] = Gasperini_atscale2["ABC.id"] + "_" + Gasperini_atscale2["GeneSymbol"]
+Gasperini_atscale2.to_csv(data_dir+"Gasperini2019.at_scale2.txt", sep='\t')
+
+
+
+ABC_gene = pd.read_csv(data_dir+"GeneList.txt", sep="\t")
+ABC_gene = ABC_gene.loc[:,['name','H3K27ac.RPKM.quantile.TSS1Kb']]
+ABC_gene_H3K4me3 = pd.read_csv(data_dir+"GeneList.H3K4me3.txt", sep="\t")
+ABC_gene_H3K4me3.rename({'H3K27ac.RPKM.quantile.TSS1Kb': 'H3K4me3.RPKM.quantile.TSS1Kb'}, axis=1, inplace=True)
+ABC_gene_H3K4me3 = ABC_gene_H3K4me3.loc[:,['name','H3K4me3.RPKM.quantile.TSS1Kb']]
+ABC_gene_H3K27me3 = pd.read_csv(data_dir+"GeneList.H3K27me3.txt", sep="\t")
+ABC_gene_H3K27me3.rename({'H3K27ac.RPKM.quantile.TSS1Kb': 'H3K27me3.RPKM.quantile.TSS1Kb'}, axis=1, inplace=True)
+ABC_gene_H3K27me3 = ABC_gene_H3K27me3.loc[:,['name','H3K27me3.RPKM.quantile.TSS1Kb']]
+ABC = pd.merge(ABC, ABC_gene, left_on=["TargetGene"], right_on=["name"], suffixes=('', '_y'))
+ABC.drop(ABC.filter(regex='_y$').columns, axis=1, inplace=True)
+ABC = pd.merge(ABC, ABC_gene_H3K4me3, left_on=["TargetGene"], right_on=["name"], suffixes=('', '_y'))
+ABC.drop(ABC.filter(regex='_y$').columns, axis=1, inplace=True)
+ABC = pd.merge(ABC, ABC_gene_H3K27me3, left_on=["TargetGene"], right_on=["name"], suffixes=('', '_y'))
+ABC.drop(ABC.filter(regex='_y$').columns, axis=1, inplace=True)
+
+# gene prediction training data 
+Gasperini_atscale_ABC = pd.merge(Gasperini_atscale2, ABC, left_on=["ABC.id"], right_on=["chr:start-end_TargetGene"], suffixes=('', '_y'))
+Gasperini_atscale_ABC.drop(Gasperini_atscale_ABC.filter(regex='_y$').columns, axis=1, inplace=True)
+Gasperini_atscale_ABC.to_csv(data_dir+"Gasperini2019.at_scale.ABC.innerjoin.txt", sep='\t')
+Gasperini_ABC_by_gene = Gasperini_atscale_ABC.groupby('TargetGene')
+Gasperini_ABC_by_gene_symbol = Gasperini_ABC_by_gene[['GeneSymbol', 'chr']].first()
+Gasperini_ABC_by_gene_sig = Gasperini_ABC_by_gene[['Significant', 'TargetGeneIsExpressed']].any()
+Gasperini_ABC_by_gene_means = Gasperini_ABC_by_gene[['TargetGeneTSS', 'TargetGeneExpression','TargetGenePromoterActivityQuantile','ABC.Score.Denominator','H3K27ac.RPKM.quantile.TSS1Kb','H3K4me3.RPKM.quantile.TSS1Kb', 'H3K27me3.RPKM.quantile.TSS1Kb']].mean()
+Gasperini_ABC_by_gene_sums = Gasperini_ABC_by_gene[['ABC.Score', 'ABC.Score.Numerator']].sum()
+Gasperini_ABC_by_genes = pd.concat([Gasperini_ABC_by_gene_symbol, Gasperini_ABC_by_gene_sig, Gasperini_ABC_by_gene_means, Gasperini_ABC_by_gene_sums], axis=1)
+
+
+
+Gasperini_atscale_ABC = pd.merge(Gasperini_atscale2, ABC, how="left", left_on=["ABC.id"], right_on=["chr:start-end_TargetGene"], suffixes=('', '_y'))
+Gasperini_atscale_ABC.drop(Gasperini_atscale_ABC.filter(regex='_y$').columns, axis=1, inplace=True)
+remove_dups_not_sig = Gasperini_atscale_ABC.duplicated('ABC.id', keep='first') & ~Gasperini_atscale_ABC['Significant']
+Gasperini_atscale_ABC = Gasperini_atscale_ABC[~remove_dups_not_sig] # remove duplicates leaving first item except when significant
+Gasperini_atscale_ABC = Gasperini_atscale_ABC[~Gasperini_atscale_ABC.duplicated('ABC.id', keep='last')]  # now remove duplicates leaving last (significant)
+Gasperini_atscale_ABC.to_csv(data_dir+"Gasperini2019.at_scale.ABC.leftjoin.txt", sep='\t')
+Gasperini_atscale_ABC = pd.merge(Gasperini_atscale2, ABC, left_on=["ABC.id"], right_on=["chr:start-end_TargetGene"], suffixes=('', '_y'))
+Gasperini_atscale_ABC.drop(Gasperini_atscale_ABC.filter(regex='_y$').columns, axis=1, inplace=True)
+remove_dups_not_sig = Gasperini_atscale_ABC.duplicated('ABC.id', keep='first') & ~Gasperini_atscale_ABC['Significant']
+Gasperini_atscale_ABC = Gasperini_atscale_ABC[~remove_dups_not_sig] # remove duplicates leaving first item except when significant
+Gasperini_atscale_ABC = Gasperini_atscale_ABC[~Gasperini_atscale_ABC.duplicated('ABC.id', keep='last')]  # now remove duplicates leaving last (significant)
+Gasperini_atscale_ABC.to_csv(data_dir+"Gasperini2019.at_scale.ABC.txt", sep='\t')
+
+
+# K562 TF
+Gasperini_atscale_ABC = pd.read_csv(data_dir+"Gasperini2019.at_scale.ABC.txt", sep='\t')
+enhancer_TF = pd.read_csv(data_dir+"Gasperini2019.enhancer.TF.overlap.bed", sep="\t", names=['chr', 'start', 'end', 'ID', 'chr.TF', 'start.TF', 'end.TF', 'TF', 'score', 'celltype', 'score2'], header=None)  
+enhancer_TF['count'] = 1
+enhancer_TF_pivot = enhancer_TF.pivot_table(index='ID', columns = 'TF', values='count', aggfunc=np.sum, fill_value=0)  
+
+TSS_TF = pd.read_csv(data_dir+"Gasperini2019.TSS.TF.overlap.bed", sep="\t", names=['chr', 'start', 'end', 'gene', 'dummy', 'strand', 'chr.TF', 'start.TF', 'end.TF', 'TF', 'score', 'celltype', 'score2'], header=None)  
+TSS_TF['count'] = 1
+TSS_TF_pivot = TSS_TF.pivot_table(index='gene', columns = 'TF', values='count', aggfunc=np.sum, fill_value=0)  
+
+TFcolumns = set().union(list(enhancer_TF_pivot.columns), list(TSS_TF_pivot.columns))  
+columns_diff = list(set(TSS_TF_pivot.columns) - set(enhancer_TF_pivot.columns))   
+for e in columns_diff:
+  enhancer_TF_pivot[e] = 0
+if columns_diff:
+  enhancer_TF_pivot = enhancer_TF_pivot.reindex(sorted(enhancer_TF_pivot.columns), axis=1)
+
+columns_diff = list(set(enhancer_TF_pivot.columns) - set(TSS_TF_pivot.columns))   
+for e in columns_diff:
+  TSS_TF_pivot[e] = 0
+if columns_diff:
+  TSS_TF_pivot = TSS_TF_pivot.reindex(sorted(TSS_TF_pivot.columns), axis=1)
+
+enhancer_TF_pivot = enhancer_TF_pivot.add_suffix('_e')
+TSS_TF_pivot = TSS_TF_pivot.add_suffix('_TSS')
+
+enhancer_TF_pivot.to_csv(data_dir+"Gasperini2019.enhancer.TF.txt", sep='\t')
+TSS_TF_pivot.to_csv(data_dir+"Gasperini2019.TSS.TF.txt", sep='\t')
+
+
+# join TF to bygene
+Gasperini_ABC_by_genes = pd.merge(Gasperini_ABC_by_genes, TSS_TF_pivot, how="left", left_on=["GeneSymbol"], right_on=["gene"], suffixes=('', '_y'))
+Gasperini_ABC_by_genes.drop(Gasperini_ABC_by_genes.filter(regex='_y$').columns, axis=1, inplace=True)
+Gasperini_ABC_by_genes.to_csv(data_dir+"Gasperini2019.bygene.ABC.TF.txt", sep='\t')
+
+# join TF to ABC
+start = Gasperini_atscale_ABC.shape[1]
+Gasperini_atscale_ABC = pd.merge(Gasperini_atscale_ABC, enhancer_TF_pivot, how="left", left_on=["enhancerID"], right_on=["ID"], suffixes=('', '_y'))
+Gasperini_atscale_ABC.drop(Gasperini_atscale_ABC.filter(regex='_y$').columns, axis=1, inplace=True)
+Gasperini_atscale_ABC = pd.merge(Gasperini_atscale_ABC, TSS_TF_pivot, how="left", left_on=["GeneSymbol"], right_on=["gene"], suffixes=('', '_y'))
+Gasperini_atscale_ABC.drop(Gasperini_atscale_ABC.filter(regex='_y$').columns, axis=1, inplace=True)
+Gasperini_atscale_ABC.to_csv(data_dir+"Gasperini2019.at_scale.ABC.TF.txt", sep='\t')
+e_TF = Gasperini_atscale_ABC.iloc[:,start:(start+len(TFcolumns))]
+TSS_TF = Gasperini_atscale_ABC.iloc[:,(start+len(TFcolumns)):(start+len(TFcolumns)+len(TFcolumns))]
+cobinding = pd.DataFrame(np.logical_and(e_TF, np.asarray(TSS_TF))).astype(int)  # ufunc(df1, np.asarray(df2)
+cobinding.columns = TFcolumns
+cobinding = cobinding.add_suffix('_co')
+Gasperini_atscale_ABC = pd.concat([Gasperini_atscale_ABC, cobinding], axis=1)
+Gasperini_atscale_ABC.to_csv(data_dir+"Gasperini2019.at_scale.ABC.TF.cobinding.txt", sep='\t')
+
+# erole
+erole = pd.read_csv(data_dir+"Gasperini2019.at_scale.eroles.txt", sep="\t")
+Gasperini_atscale_ABC = pd.merge(Gasperini_atscale_ABC, erole, left_on=["name"], right_on=["name_x"])
+Gasperini_atscale_ABC.drop(Gasperini_atscale_ABC.filter(regex='_x$').columns, axis=1, inplace=True)
+Gasperini_atscale_ABC.to_csv(data_dir+"Gasperini2019.at_scale.ABC.TF.cobinding.erole.txt", sep='\t')
+
+
